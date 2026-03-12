57bee3e439225abdf8338e31f59bd7aee615acd6
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-03-13 00:15:36 +0530

    Add files via upload

---
+++ generate_cert.py
@@ -0,0 +1,45 @@
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import rsa
+from datetime import datetime, timedelta
+
+# generate private key
+key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048
+)
+
+# generate certificate
+subject = issuer = x509.Name([
+    x509.NameAttribute(NameOID.COUNTRY_NAME, "US"),
+    x509.NameAttribute(NameOID.ORGANIZATION_NAME, "PubSubProject"),
+    x509.NameAttribute(NameOID.COMMON_NAME, "localhost"),
+])
+
+cert = (
+    x509.CertificateBuilder()
+    .subject_name(subject)
+    .issuer_name(issuer)
+    .public_key(key.public_key())
+    .serial_number(x509.random_serial_number())
+    .not_valid_before(datetime.utcnow())
+    .not_valid_after(datetime.utcnow() + timedelta(days=365))
+    .sign(key, hashes.SHA256())
+)
+
+# write key
+with open("server.key", "wb") as f:
+    f.write(
+        key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.TraditionalOpenSSL,
+            encryption_algorithm=serialization.NoEncryption(),
+        )
+    )
+
+# write certificate
+with open("server.crt", "wb") as f:
+    f.write(cert.public_bytes(serialization.Encoding.PEM))
+
+print("Certificate and key generated!")
